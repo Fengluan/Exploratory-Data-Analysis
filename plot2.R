7b77657bd10b65b717d18c712d9aa399bce73809
Fengluan <693264936@qq.com>
2014-12-05 09:07:58 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,8 @@
 library(lubridate)
 hpc <- read.table("household_power_consumption.txt", sep = ";", header = T)
 hpc$Global_active_power <- as.numeric(as.character(hpc$Global_active_power))
-t <- paste(hpc$Date, hpc$Time)
+y <- hpc[wday(hpc$Date) == 5 | wday(hpc$Date) == 6 | wday(hpc$Date) == 7,]
+y$wday_time <- strptime(paste(wday(y$Date, label = T, abbr = T), y$Time), "%a %H:%M:%S")
+png("plot2.png")
+plot(y$wday_time, y$Global_active_power, type = "l")
+dev.off()
