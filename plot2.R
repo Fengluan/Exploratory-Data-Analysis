7538ec42db90d2ef304998c78e22d1d86e4160c5
Fengluan <693264936@qq.com>
2014-12-05 09:43:30 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,5 +4,5 @@ hpc$Global_active_power <- as.numeric(as.character(hpc$Global_active_power))
 y <- hpc[wday(hpc$Date) == 5 | wday(hpc$Date) == 6 | wday(hpc$Date) == 7,]
 y$wday_time <- strptime(paste(wday(y$Date, label = T, abbr = T), y$Time), "%a %H:%M:%S")
 png("plot2.png")
-plot(y$wday_time, y$Global_active_power, type = "l")
+plot(y$wday_time, y$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)")
 dev.off()
